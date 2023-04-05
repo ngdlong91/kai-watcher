53f7e8a8d40e93b741ae1a21dcea33912313f766
ngdlong91 <ngdlong91@gmail.com>
2023-04-05 17:58:03 +0700

    Update trigger point

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"github.com/joho/godotenv"
+	"github.com/ngdlong91/kai-watcher/watcher/staking"
+	"github.com/ngdlong91/kai-watcher/watcher/validator"
+	"github.com/ngdlong91/kai-watcher/watcher/whale"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
@@ -77,4 +80,93 @@ func main() {
 
 	<-waitExit
 	logger.Info("Stopped")
+}
+
+func WatchStakingSMC(ctx context.Context, interval time.Duration) {
+	lgr := logger.With(zap.String("Watcher", "Staking"))
+	cfg := staking.Config{
+		URL:            gCfg.KardiaTrustedNodes[0],
+		Logger:         logger,
+		AlertToken:     gCfg.TelegramToken,
+		StakingAddress: gCfg.StakingAddress,
+	}
+	watcher, err := staking.NewWatcher(cfg)
+	if err != nil {
+		lgr.Error("cannot create watcher", zap.Error(err))
+		panic(err)
+	}
+	lgr.Info("Start staking watcher")
+	t := time.NewTicker(interval)
+	defer t.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			if err := watcher.Run(ctx); err != nil {
+				continue
+			}
+		}
+	}
+}
+
+func WatchValidators(ctx context.Context, interval time.Duration) {
+	lgr := logger.With(zap.String("Watcher", "Validators"))
+	cfg := validator.Config{
+		URL:        gCfg.KardiaTrustedNodes[0],
+		Logger:     logger,
+		AlertToken: gCfg.TelegramToken,
+	}
+	watcher, err := validator.NewWatcher(cfg)
+	if err != nil {
+		lgr.Error("cannot create watcher", zap.Error(err))
+		panic(err)
+	}
+	lgr.Info("Start watch validator")
+	t := time.NewTicker(interval)
+	defer t.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			if err := watcher.Run(ctx); err != nil {
+				lgr.Error("Run error", zap.Error(err))
+				continue
+			}
+		}
+	}
+}
+
+func WatchWhaleTransaction(ctx context.Context, interval time.Duration) {
+	lgr := logger.With(zap.String("Watcher", "WhaleAlert"))
+	cfg := whale.Config{
+		URL:             gCfg.KardiaTrustedNodes[0],
+		Logger:          logger,
+		AlertToken:      gCfg.TelegramToken,
+		LevelOneLimit:   gCfg.LevelOneLimit,
+		LevelTwoLimit:   gCfg.LevelTwoLimit,
+		LevelThreeLimit: gCfg.LevelThreeLimit,
+		LevelFourLimit:  gCfg.LevelFourLimit,
+	}
+	watcher, err := whale.NewWatcher(cfg)
+	if err != nil {
+		lgr.Error("cannot create watcher", zap.Error(err))
+		panic(err)
+	}
+	lgr.Info("Start whale alert watcher")
+	t := time.NewTicker(interval)
+	defer t.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			lgr.Info("Tick")
+			if err := watcher.Run(ctx); err != nil {
+				lgr.Error("watcher err", zap.Error(err))
+				continue
+			}
+		}
+	}
 }
