c807f7bef40044eeee1c3a037129951b3d8d4848
ngdlong91 <ngdlong91@gmail.com>
2023-04-05 16:24:30 +0700

    Init

--- cmd/ticker/main.go
+++ cmd/ticker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/joho/godotenv"
+	"github.com/ngdlong91/kai-watcher/watcher/whale"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
@@ -77,4 +78,37 @@ func main() {
 
 	<-waitExit
 	logger.Info("Stopped")
+}
+
+func watchWhaleTransaction(ctx context.Context, interval time.Duration) {
+	lgr := logger.With(zap.String("Watcher", "WhaleAlert"))
+	cfg := whale.Config{
+		URL:             gCfg.KardiaTrustedNodes[0],
+		Logger:          logger,
+		AlertToken:      gCfg.TelegramToken,
+		LevelOneLimit:   gCfg.LevelOneLimit,
+		LevelTwoLimit:   gCfg.LevelTwoLimit,
+		LevelThreeLimit: gCfg.LevelThreeLimit,
+		LevelFourLimit:  gCfg.LevelFourLimit,
+	}
+	watcher, err := whale.NewWatcher(cfg)
+	if err != nil {
+		lgr.Error("cannot create watcher", zap.Error(err))
+		panic(err)
+	}
+	lgr.Info("Start whale alert watcher")
+	t := time.NewTicker(interval)
+	defer t.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			lgr.Info("Tick")
+			if err := watcher.Run(ctx); err != nil {
+				lgr.Error("watcher err", zap.Error(err))
+				continue
+			}
+		}
+	}
 }
